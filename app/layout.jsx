18e8d9f5734505a0b224b010b2b2f78188a56238
cmattox1983 <chris@mattoxsfg.com>
2026-02-05 14:15:54 -0500

    Set site title to Skinstric

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,11 @@ import "./globals.css";
 import { roobert } from "./fonts";
 import Navbar from "@/components/Navbar";
 
+export const metadata = {
+  title: "Skinstric",
+  description: "Skinstric AI Internship",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
