2e6a605724feaeea093a70485a378b877b510ba0
cmattox1983 <chris@mattoxsfg.com>
2026-04-15 08:36:31 -0400

    Add Vercel Speed Insights

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { roobert } from "./fonts";
 import Navbar from "@/components/Navbar";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
   title: "Skinstric",
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
         <Navbar />
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
