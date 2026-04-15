e76c124e96406f3c0423af6a48746f8bfefaf910
cmattox1983 <chris@mattoxsfg.com>
2026-04-15 08:05:01 -0400

    Fix Vercel Analytics import

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { roobert } from "./fonts";
 import Navbar from "@/components/Navbar";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "Skinstric",
